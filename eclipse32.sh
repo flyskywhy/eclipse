9a46ce6bee6a08546b1f67e0b1c204ad73995a24
Li Zheng <flyskywhy@gmail.com>
2013-06-18 15:09:04 +0800

    Can run eclipse32.sh at any path now

--- eclipse32.sh
+++ eclipse32.sh
@@ -3,4 +3,4 @@
 # Author: Li Zheng <flyskywhy@gmail.com>
 # Run this 32-bit eclipse with 32-bit java in 64-bit Linux.
 
-./eclipse -vm "../eps/mydroid/jdk1.5.0_18/jre/bin/java"
+$(cd "$(dirname "$0")"; pwd)/eclipse -vm "../eps/mydroid/jdk1.5.0_18/jre/bin/java"
